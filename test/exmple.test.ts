eedf0464b16346c4cb12d213883c07bd9e28e5e8
cenfun <cenfun@gmail.com>
2024-03-16 22:55:42 +0800

    add case

--- test/exmple.test.ts
+++ test/exmple.test.ts
@@ -1,7 +1,15 @@
 import { bar } from '../src/example.js';
+import { add } from '../src/index.js';
 
 describe('test bar', function() {
+
     it('test', function() {
         bar();
     });
+
+    it('add', () => {
+        add(1, 1);
+    });
+
 });
+
